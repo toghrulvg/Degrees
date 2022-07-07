2bc7090dd1fe2eecf0e7390b9b9d0cccaa57e518
toghrulvg <toghrulvg@code.edu.az>
2022-07-07 22:00:18 +0400

    Committed

---
+++ Degrees/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celcius celcius = new Celcius(18);
+
+            Kelvin kelvin = celcius;
+
+            Console.WriteLine(kelvin.Klv + "K");
+
+
+        }
+    }
+
+    public class Kelvin
+    {
+        public float Klv { get; set; }
+
+        public Kelvin(float kelvin)
+        {
+            Klv = kelvin;
+        }
+    }
+
+    public class Celcius
+    {
+        public float Clc { get; set; }
+
+        public Celcius(float celcius)
+        {
+            Clc = celcius;
+        }
+
+        public static implicit operator Kelvin(Celcius celcius)
+        {
+            return new Kelvin(celcius.Clc + 270);
+        }
+    }
+}
